3625d6dd5424131cfc65b377cece0d631bc24736
somguyoit <jose.morenodedios@oit.edu>
2018-02-22 22:48:01 -0800

    added agents to server working on socket connection

--- Client/client.py
+++ Client/client.py
@@ -2,18 +2,33 @@
 """
 Currently basic agent prototype to check the observations given, and log them.
 """
+import socket, pickle, sys
 from pysc2.agents import base_agent
 from pysc2.lib import actions
 
+target = 'localhost'
+port = 2323
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 class ConnectAgent(base_agent.BaseAgent):
-    def __init__(self, agent):
+    def __init__(self):
         super(ConnectAgent,self).__init__()
-        self.agent = agent
-        self.connection = None
+        # self.agent = agent
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((target,port))
+        self.id = self.connection.recv(8)
+        print(self.id.decode())
 
     def step(self, obs):
         super(ConnectAgent,self)
-        with open("logfile.txt", 'a') as logfile:
-            logfile.write(str(obs))
+        data = pickle.dumps(obs)
+        self.connection.send(str(sys.getsizeof(data)).encode())
+        # print(sys.getsizeof(data))        
+        self.connection.sendall(data)
+        # self.connection.send(b'')
+        # with open("logfile.txt", 'a') as logfile:
+        #     logfile.write(str(obs))
+        readSize = int(self.connection.recv(sys.getsizeof(int)))
+        action = pickle.loads(self.connection.recv(readSize))
+        print(readSize,'\n',action)
 
-        return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
+        return action
---
+++ Server/server.py
@@ -0,0 +1,46 @@
+# import sys
+# sys.path.insert(0,'./pysc2-tutorial/Building a Sparse Reward Agent')
+import socket, pickle, sys
+import threading
+from agents.sparse_agent import SparseAgent
+
+bind_ip = 'localhost'
+bind_port = 2323
+agent = SparseAgent()
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip,bind_port))
+
+server.listen(5)  # max backlog of connections
+
+print ('Listening on {}:{}'.format(bind_ip, bind_port))
+
+def handle_client_connection(client_socket):
+    client_socket.send(str(threading.get_ident()).encode())
+    print(threading.get_ident())
+    # chunk = client_socket.recv(4096)
+    # obs = chunk
+    # print(chunk)
+    # while True:
+    while True:
+        readSize = int(client_socket.recv(sys.getsizeof(int)))
+        print(readSize)
+        chunk = client_socket.recv(readSize)
+        obs = pickle.loads(chunk)
+        print('sending')
+        action = agent.step(obs)
+        print(action)
+        action = pickle.dumps(action)
+        readSize = sys.getsizeof(action)
+        client_socket.send(str(sys.getsizeof(action)).encode())
+        print(readSize)
+        client_socket.sendall(action)
+        obs = None
+    # print(obs)
+
+while True:
+    client_socket, address = server.accept()
+    print("connected to {}:{}".format(address[0],address[1]))
+    client_handler = threading.Thread(target=handle_client_connection, args=(client_socket,))
+    client_handler.start()
+
